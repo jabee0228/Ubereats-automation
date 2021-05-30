3592e9d6faf23fe89d35c4bfda717008ca097689
jabee0228 <james930228@gmail.com>
2021-05-30 14:38:07 +0800

    update bs4

--- my.py
+++ my.py
@@ -9,13 +9,13 @@ def run(playwright):
 
     # Go to https://www.google.com/
     page.goto("https://www.google.com/")
-
+    page.wait_for_timeout(5000)
     # Click [aria-label="搜尋"]
     page.click("[aria-label=\"搜尋\"]")
-
+    page.wait_for_timeout(5000)
     # Fill [aria-label="搜尋"]
     page.fill("[aria-label=\"搜尋\"]", "jamee")
-
+    page.wait_for_timeout(5000)
     # Click text=Google 搜尋
     # with page.expect_navigation(url="https://www.google.com/search?q=jamee&source=hp&ei=-w-zYL-hDcHLmAWr4LK4AQ&iflsig=AINFCbYAAAAAYLMeC7F0xhIrrQQK0QqDaiZKZv2i6Fp-&oq=jamee&gs_lcp=Cgdnd3Mtd2l6EAwyAggAMgIIADICCAAyAggAMgIIADICCAAyAggAMgIIADICCAAyAggAOggIABCxAxCDAToFCAAQsQM6CggAELEDEIMBEApQsUhYmldg0HdoAXAAeACAATuIAecBkgEBNZgBAKABAaoBB2d3cy13aXqwAQA&sclient=gws-wiz&ved=0ahUKEwj_iuy0xPDwAhXBJaYKHSuwDBcQ4dUDCAk"):
     with page.expect_navigation():
@@ -23,8 +23,8 @@ def run(playwright):
     # assert page.url == "https://www.google.com/search?q=jamee&source=hp&ei=-w-zYL-hDcHLmAWr4LK4AQ&iflsig=AINFCbYAAAAAYLMeC7F0xhIrrQQK0QqDaiZKZv2i6Fp-&oq=jamee&gs_lcp=Cgdnd3Mtd2l6EAwyAggAMgIIADICCAAyAggAMgIIADICCAAyAggAMgIIADICCAAyAggAOggIABCxAxCDAToFCAAQsQM6CggAELEDEIMBEApQsUhYmldg0HdoAXAAeACAATuIAecBkgEBNZgBAKABAaoBB2d3cy13aXqwAQA&sclient=gws-wiz&ved=0ahUKEwj_iuy0xPDwAhXBJaYKHSuwDBcQ4dUDCAk"
 
     # ---------------------
-    context.close()
-    browser.close()
+    # context.close()
+    # browser.close()
 
 with sync_playwright() as playwright:
     run(playwright)
